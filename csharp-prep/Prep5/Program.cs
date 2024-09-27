1c2b491bfe13525b2856cdbcc7d7bee64b284317
isaacpacas <isaacpacas@gmail.com>
2024-09-26 23:56:23 -0500

    activity prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        initialmessage();
+        string fuctionname = username();
+        int functionnumber = favoritenumber();
+        int funtionsquare = squarenumber(functionnumber);
+        result(fuctionname, funtionsquare);
+
+
+
+        static void initialmessage()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+         static string username()
+        {
+            Console.Write("Please enter your name: ");
+            string nameoftheuser = Console.ReadLine();
+            return nameoftheuser;
+        }
+
+        static int favoritenumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string numberoftheuser = Console.ReadLine();
+            int numberoftheuserconverted = int.Parse(numberoftheuser);
+            return numberoftheuserconverted;
+        }
+
+        static int squarenumber(int numberoftheuserconverted)
+        {
+            int squareofthenumber = numberoftheuserconverted*numberoftheuserconverted;
+            return squareofthenumber;
+        }
+
+        static void result(string nameoftheuser, int squareofthenumber )
+        {
+            Console.WriteLine($"{nameoftheuser}, the square of your number is {squareofthenumber}");
+            
+        }
+
     }
 }
