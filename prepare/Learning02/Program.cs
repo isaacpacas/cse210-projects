ef420a4c4f1736d203a94d5a4264ad67ff5e6e61
isaacpacas <isaacpacas@gmail.com>
2024-10-07 20:57:47 -0500

    Abstraction Learning Activity.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+       
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //job1.Display();
+        //job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Alisson Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+       
+
     }
 }
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,24 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Nme: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach(Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+    
+
+
+
+
+}
